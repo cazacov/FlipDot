56669aba06548b4f5f9c65864d6fb6a7e68036e4
Victor Cazacov <cazacov.git@gmail.com>
2021-01-01 18:38:31 +0100

    Nyan cat demo

--- Brose_63x19/Master/Esp8266-GFX/src/flipBoard.h
+++ Brose_63x19/Master/Esp8266-GFX/src/flipBoard.h
@@ -6,9 +6,6 @@
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_GFX.h>
 
-#define PANEL_WIDTH 21
-#define PANEL_HEIGHT 19
-
 struct Panel {
   uint8_t i2cAddress;
   uint8_t resetPin;
@@ -16,18 +13,27 @@ struct Panel {
 
 class FlipBoard : public Adafruit_GFX {
 private:
-  uint8_t pins[10];
-  uint8_t panels[10];
+  uint8_t *resetPins;
+  uint8_t *panelAddrs;
   uint8_t rows;
   uint8_t columns;
   uint8_t panelCount;
-  int maxx;
-  int maxy;
+  uint8_t panelWidth;
+  uint8_t panelHeight;
+  uint16_t maxx;
+  uint16_t maxy;
+
+  uint8_t* frameBuffer;
+  uint8_t* screenBuffer;
+  uint16_t frameBufferSize;
+  uint16_t frameBufferWidth;
+
   void sendCommand(uint8_t panel, uint8_t command, uint8_t param1, uint8_t param2);
-  void dot_set(uint8_t x, uint8_t y);
-  void dot_reset(uint8_t x, uint8_t y);
+  void dot_set(uint16_t x, uint16_t y);
+  void dot_reset(uint16_t x, uint16_t y);
+  void writeDot(int16_t x, int16_t y, bool state);
 public:
-  FlipBoard(Panel panelInfo[], uint8_t prows, uint8_t pcolumns);
+  FlipBoard(uint8_t panel_width, uint8_t panel_height, Panel panelInfo[], uint8_t prows, uint8_t pcolumns);
   void begin();
   void resetPanels();
   void test();
@@ -36,7 +42,9 @@ public:
   void sleep();
   // Override AdafruitGFX
   void drawPixel(int16_t x, int16_t y, uint16_t color);
+  void writePixel(int16_t x, int16_t y, uint16_t color);
   void fillScreen(uint16_t color);
+  void endWrite(void);  
 };
 
 #endif
