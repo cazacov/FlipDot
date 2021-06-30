64ade8e266041d10c01c02299c28f3be55d67400
Victor Cazacov <cazacov.git@gmail.com>
2021-06-30 23:58:34 +0200

    Export font in Adafruit GFX format

---
+++ BitViewer/src/FontExporter/Font.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace FontExporter
+{
+    internal class Font
+    {
+        public int FontCode;
+        public SortedList<int, Character> Characters = new SortedList<int, Character>();
+    }
+}
---
+++ BitViewer/src/FontExporter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Mime;
+
+namespace FontExporter
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Please provide file name");
+                return -1;
+            }
+
+            var parser = new FontsParser();
+            var bytes = System.IO.File.ReadAllBytes(args[0]);
+            parser.Parse(bytes);
+            var x = parser.Fonts;
+
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+}
