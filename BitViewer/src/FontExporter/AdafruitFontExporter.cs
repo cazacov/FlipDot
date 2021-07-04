71a83d2fc1ba2e17e0b4c983fed4ba6b308bb731
Victor Cazacov <cazacov.git@gmail.com>
2021-07-04 23:51:36 +0200

    Fonts exporter

---
+++ BitViewer/src/FontExporter/AdafruitFontExporter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FontExporter
+{
+    class AdafruitFontExporter
+    {
+        public void Export(string fileName, string fontName, Font font, Character refChar)
+        {
+            ;
+        }
+    }
+}
--- BitViewer/src/FontExporter/Program.cs
+++ BitViewer/src/FontExporter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Net.Mime;
 
 namespace FontExporter
@@ -14,11 +16,44 @@ namespace FontExporter
             }
 
             var parser = new FontsParser();
+            var exporter = new AdafruitFontExporter();
+
+            if (!System.IO.Directory.Exists(args[1]))
+            {
+                System.IO.Directory.CreateDirectory(args[1]);
+            }
+            
             var bytes = System.IO.File.ReadAllBytes(args[0]);
             parser.Parse(bytes);
-            var x = parser.Fonts;
 
-            Console.WriteLine("Hello World!");
+            foreach (var font in parser.Fonts.Values)
+            {
+                var maxHeight = font.Characters.Values.Max(x => x.Height);
+                var minCode = font.Characters.Values.Min(x => x.Code);
+                var maxCode = font.Characters.Values.Max(x => x.Code);
+                Character refChar;
+                if (font.Characters.ContainsKey(65))
+                {
+                    refChar = font.Characters[65];
+                }
+                else
+                {
+                    refChar = font.Characters.First().Value;
+                }
+                Console.Write($"{font.FontCode} - {font.Characters.Count}  [{minCode}-{maxCode}]");
+
+                if (font.Characters.Count > 20)
+                {
+                    var targetDir = args[1];
+                    var fontName = $"bus_{font.FontCode}_{refChar.Width}x{maxHeight}";
+                    exporter.Export(System.IO.Path.Combine(targetDir, fontName) + ".h", fontName, font, refChar);
+                    Console.WriteLine($" -> {fontName}");
+                }
+                else
+                {
+                    Console.WriteLine($" skipped");
+                }
+            }
             return 0;
         }
     }
