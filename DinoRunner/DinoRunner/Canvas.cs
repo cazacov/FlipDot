f2077b689857b782fd201389507922f6d6069e3d
Victor Cazacov <cazacov.git@gmail.com>
2023-01-11 23:59:04 +0100

    Implement jumps

--- DinoRunner/DinoRunner/Canvas.cs
+++ DinoRunner/DinoRunner/Canvas.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace DinoRunner
 {
@@ -12,8 +21,8 @@ namespace DinoRunner
 
     internal class Canvas
     {
-        private const int Height = 140;
-        private const int Width = 140;
+        public const int Height = 19;
+        public const int Width = 140;
 
         private bool[,] Pixels = new bool[Height, Width];
 
@@ -24,9 +33,9 @@ namespace DinoRunner
 
         public void DrawBaseLine()
         {
-            for (var x = 0; x < Height; x++)
+            for (var x = 0; x < Width; x++)
             {
-                Pixels[17, x] = true;
+                Pixels[16, x] = true;
             }
         }
 
@@ -84,19 +93,111 @@ namespace DinoRunner
                     Pixels[y + 8, x + 5] = true;
                     break;
             }
+            Pixels[y + 6, x] = false;
             Pixels[y + 6, x + 1] = false;
             Pixels[y + 7, x + 1] = false;
             Pixels[y + 7, x + 2] = false;
             Pixels[y + 8, x + 2] = false;
+
             Pixels[y + 6, x + 6] = false;
+            Pixels[y + 6, x + 7] = false;
             Pixels[y + 7, x + 6] = false;
             Pixels[y + 7, x + 7] = false;
             Pixels[y + 8, x + 6] = false;
         }
 
-        public void Display(string url)
+        public async Task Display(string url)
         {
-            ;
+            var bytesperRow = (Width + 7) >> 3;
+            var bytes = new byte[bytesperRow * Height];
+
+            for (var y = 0; y < Height; y++)
+            {
+                var mask = 0x80;
+                var index = bytesperRow * y;
+                for (var x = 0; x < Width; x++)
+                {
+                    if (Pixels[y, x])
+                    {
+                        bytes[index] = (byte)(bytes[index] | mask);
+                    }
+                    mask >>= 1;
+                    if (mask == 0)
+                    {
+                        index++;
+                        mask = 0x80;
+                    }
+                }
+            }
+
+            var encoded = Convert.ToBase64String(bytes);
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+
+            var str = JsonConvert.SerializeObject(
+                new
+                {
+                    frameBuffer = encoded
+                }
+            );
+            var content = new StringContent(str, Encoding.UTF8, "application/json");
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            var result = await client.PostAsync("data", content);
+        }
+
+        public void DrawStones(List<Pos> stones)
+        {
+            foreach (var stone in stones)
+            {
+                PutPixel(stone.X, stone.Y);
+            }
+        }
+
+        private void PutPixel(int x, int y)
+        {
+            if (x is >= 0 and < Width && y is >= 0 and < Height)
+            {
+                Pixels[y, x] = true;
+            }
+        }
+
+        public void DrawCacti(List<Cactus> cacti)
+        {
+            foreach (var cactus in cacti)
+            {
+                var x = cactus.X;
+                var y = cactus.Y;
+
+                PutBar(x + 2, y, 2, 8);
+
+                switch (cactus.Type)
+                {
+                    case CactusType.Type1:
+                        PutBar(x, y + 2, 1, 3);
+                        PutPixel(x + 1, y + 4);
+                        PutPixel(x + 4, y+3);
+                        PutBar(x + 5, y+1, 1, 3);
+                        break;
+                    case CactusType.Type2:
+                        PutBar(x, y + 1, 1, 4);
+                        PutPixel(x + 1, y + 4);
+                        PutPixel(x + 4, y + 4);
+                        PutBar(x + 5, y + 2, 1, 3);
+                        break;
+                }
+            }
+        }
+
+        private void PutBar(int x, int y, int w, int h)
+        {
+            for (var xx = x; xx < x + w; xx++)
+            {
+                for (var yy = y; yy < y + h; yy++)
+                {
+                    PutPixel(xx,yy);
+                }
+            }
         }
     }
 }
--- DinoRunner/DinoRunner/Program.cs
+++ DinoRunner/DinoRunner/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,23 +8,47 @@ namespace DinoRunner
 {
     internal class Program
     {
+        static Random random = new Random();
+
         static async Task Main(string[] args)
         {
+
+            var url = "http://192.168.178.61/";
+
             Console.WriteLine("Dino run. Press Esc to exit");
             var canvas = new Canvas();
 
-            State state = State.Step1;
+            var state = State.Step1;
+            var jump = 0;
+
+            var stones = new List<Pos>();
+            var cacti = new List<Cactus>();
+
+            canvas.Cls();
+            canvas.DrawBaseLine();
+            await canvas.Display(url);
+            await Task.Delay(3000);
+
+            canvas.PutDino(60, 10, State.Step1);
+            await canvas.Display(url);
+            await Task.Delay(300);
 
             do
             {
                 canvas.Cls();
                 canvas.DrawBaseLine();
+                canvas.DrawStones(stones);
+                canvas.DrawCacti(cacti);
 
                 var x = 60;
                 var y = 10;
 
-                Console.WriteLine($"{x},{y}\t{state}");
-                canvas.PutDino(60, 10, state);
+                var mustJump = state != State.Jump && cacti.Any(c => c.X == x + 14);
+                if (mustJump)
+                {
+                    state = State.Jump;
+                    jump = 26;
+                }
 
                 switch (state)
                 {
@@ -32,11 +58,81 @@ namespace DinoRunner
                     case State.Step2:
                         state = State.Step1;
                         break;
+                    case State.Jump:
+                        jump--;
+                        if (jump == 0)
+                        {
+                            state = State.Step1;
+                            y = 10;
+                        }
+                        else
+                        {
+                            if (jump < 11)
+                            {
+                                y = 10 - jump;
+                            }
+                            else if (jump < 15)
+                            {
+                                y = 0;
+                            }
+                            else
+                            {
+                                y = jump - 15;
+                            }
+                        }
+                        break;
                 }
 
-                canvas.Display("http:://localhost:5000/");
+                Console.WriteLine($"{x},{y}\t{state}");
+                canvas.PutDino(x, y, state);
+
+                await canvas.Display(url);
+
+                UpdateStones(stones, canvas);
+                UpdateCacti(cacti, canvas);
+
+
                 await Task.Delay(100);
             } while (!Console.KeyAvailable  || Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        private static void UpdateCacti(List<Cactus> cacti, Canvas canvas)
+        {
+            foreach (var cactus in cacti)
+            {
+                cactus.X = cactus.X - 1;
+            }
+            cacti.RemoveAll(cactus => cactus.X < -3);
+
+            if (cacti.Any())
+            {
+                var maxX = cacti.Max(c => c.X);
+                if (maxX + 27 >= Canvas.Width)
+                {
+                    return;
+                }
+            }
+
+            if (random.Next(20) == 0)
+            {
+                var type = random.Next(10) < 5 ? CactusType.Type1 : CactusType.Type2;
+                cacti.Add(new Cactus(Canvas.Width + 1, Canvas.Height - 9, type));
+            }
+        }
+
+        private static void UpdateStones(List<Pos> stones, Canvas canvas)
+        {
+            foreach (var stone in stones)
+            {
+                stone.X = stone.X - 1;
+            }
+            stones.RemoveAll(stone => stone.X < 0);
+
+            if (random.Next(10) == 0)
+            {
+                var y = random.Next(10) < 5 ? Canvas.Height - 1 : Canvas.Height - 2;
+                stones.Add(new Pos(Canvas.Width - 1, y));
+            }
         }
     }
 }
